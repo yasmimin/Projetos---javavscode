770f23c09d4d51b71f4d4254e66049a9d27ae6f3
yasmin <15004114929@seagri.df.gov.br>
2024-09-05 15:50:08 -0300

    COMMIT 3 AULA 2

---
+++ Aula02/numerosVariaveis.java
@@ -0,0 +1,17 @@
+package Aula02;
+
+public class numerosVariaveis {
+
+    //EXEMPLOS DA AULA 2 DE VARIÁVEIS
+
+    int idade; //Tipo "int", nome "idade", com nenhum valor atribuído. 
+    int anoFabricacao = 2021; //tipo "int", nome "anoFabricacao", com valor 2021.
+    double salarioMinimo = 2.500; //tipo "double", nome "salarioMinimo", valor 2.500.
+
+    //Observe que o tipo long precisa terminar com L, o tipo float precisa terminar com F 
+    //e alguns cenários do dia-a-dia podem estimular uma alteração de tipos de dados convencional.
+
+
+
+
+}
