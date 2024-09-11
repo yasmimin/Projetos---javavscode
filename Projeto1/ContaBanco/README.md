2ae7052853c345c14dd1bde3465fa5a576c49ad8
yasmin <15004114929@seagri.df.gov.br>
2024-09-11 13:43:44 -0300

    commit

--- Projeto1/ContaBanco/README.md
+++ Projeto1/ContaBanco/README.md
@@ -1,18 +1,11 @@
-## Getting Started
+## Projeto JAVA Básico 
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Primeiro desafio de projeto em código do curso DIO-trilha-java-basico
 
-## Folder Structure
+## Sobre o projeto
 
-The workspace contains two folders by default, where:
+Nesse projeto ContaBanco somos desafiados a inserir dados no terminal do vscode, contendo características de conta em banco.
 
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
+## Informações sobre o projeto
 
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+Informações sobre esse desafio podem ser encontrados [aqui](https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/sintaxe)
--- Projeto1/ContaBanco/src/ContaTerminal.java
+++ Projeto1/ContaBanco/src/ContaTerminal.java
@@ -16,21 +16,28 @@ public class ContaTerminal {
         System.out.println("Digite o nome da agência: ");
        String agencia = scanner.next();
 
+       //Println para retornar o número da conta do cliente
         System.out.println("Digite o número da sua Conta: ");
         int numero = scanner.nextInt();
 
+        //Println para retornar o número do saldo do cliente
         System.out.println("Digite o número da seu saldo: ");
         double saldo = scanner.nextDouble();
 
+        //Println para digitar o nome do cliente
         System.out.println("Digite seu nome: ");
         scanner.nextLine();
         String nomeCliente = scanner.nextLine();
+        //Fim da solicitação de dados ao usuário
 
-
+        
+        //String para retornar mensagem para o usuário
         String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque. ";
 
+        //Print para exibir no console a mensagem para o usuário
         System.out.println(mensagem);
 
+        //Fechamento de Scanner
         scanner.close();
 
 
